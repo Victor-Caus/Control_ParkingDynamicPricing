afa295a87b2998c581c11e68fbba1388317d2bad
gubidubi <gubidu0@gmail.com>
2024-07-13 14:27:43 -0300

    Teste controlador PI arbitrario

--- avaliarControladorAnalitico.m
+++ avaliarControladorAnalitico.m
@@ -1,2 +1,13 @@
+clear
+planta = obterPlantaProjeto();
 s = tf('s');
-Gf = (Ki*f*a/(1-Kp*a))/(s^2 + s*(f + Kp*f*a - Ki*a)/(1-Kp*a) + (Ki*a*f)/(1-Kp*a));
+Ki = 2.3289e-6;
+Kp = 1;
+tau = 86400/2;
+C = Ki/s + Kp;
+delay = exp(-tau*s);
+%[num, den] = pade(tau, 1);
+%delay = tf(num, den);
+Ga = C * delay * (-planta.a);
+Gf = minreal(feedback(Ga,1));
+margin(Gf);
