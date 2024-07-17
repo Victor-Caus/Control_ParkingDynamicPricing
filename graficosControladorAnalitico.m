d2589c681ee09d90a0469a6d00b3c78676cde332
gabrielcbomfim <gabrielcaetite@hotmail.com>
2024-07-16 22:32:18 -0300

    main atualizada

--- graficosControladorAnalitico.m
+++ graficosControladorAnalitico.m
@@ -8,27 +8,25 @@ wb = 2*pi./Tb;
 
 Kp = 0:1e-5:1e1;
 
-% Relation Ki x Kp for fixed PM
+% Relação Ki x Kp para PM fixo
 Ki1 = (pi/180).*(90 - PM + atand(a^2*Kp.^2./(1 - a.^2*Kp.^2))).*(sqrt((1-a.^2.*Kp.^2)/a.^2));
 
-% Relation Ki x Kp for fixed wb
+% Relação Ki x Kp para wb fixo
 Ki2 = sqrt(2.*wb.^2/a.^2 - (Kp.*wb).^2);
 
-% Function to find the intersection
+% Função para encontrar a interseção
 fun = @(Kp) (pi/180)*(90 - PM + atand(a^2*Kp.^2./(1 - a.^2*Kp.^2))).*(sqrt((1-a.^2.*Kp.^2)/a.^2)) - sqrt(2*wb.^2/a.^2 - (Kp*wb).^2);
 
-% Initial guess for fzero
+% Encontrar a interseção ( poderia ser visualmente no grafico)
 Kp_initial_guess = 1;
-
-% Find the intersection
 Kp_intersect = fzero(fun, Kp_initial_guess);
 Ki_intersect = (pi/180)*(90 - PM + atand(a^2*Kp_intersect.^2./(1 - a.^2*Kp_intersect.^2))).*(sqrt((1-a.^2.*Kp_intersect.^2)/a.^2));
 
-% Plot the curves and the intersection
-plot(Kp, Ki1, 'LineWidth', 2, 'DisplayName', 'Fixed PM');
+% Plotar as curvas e a interseção
+plot(Kp, Ki1, 'LineWidth', 2, 'DisplayName', 'PM fixo');
 hold on;
-plot(Kp, Ki2, 'LineWidth', 2, 'DisplayName', 'Fixed wb');
-plot(Kp_intersect, Ki_intersect, 'ro', 'MarkerSize', 8, 'DisplayName', 'Intersection');
+plot(Kp, Ki2, 'LineWidth', 2, 'DisplayName', 'wb fixo');
+plot(Kp_intersect, Ki_intersect, 'ro', 'MarkerSize', 8, 'DisplayName', 'Interseção');
 xlabel('Kp', 'FontSize', 14);
 ylabel('Ki', 'FontSize', 14);
 set(gca, 'FontSize', 14);
@@ -36,6 +34,6 @@ grid on;
 legend('show', 'Location', 'best');
 print -depsc2 metodoGrafico.eps
 
-% Intersection found
+% Interseção encontrada
 Kp_intersect;
 Ki_intersect;
--- main.m
+++ main.m
@@ -5,14 +5,18 @@ planta = obterPlanta();
 requisitos = obterRequisitos();
 controlador_analitico = projetarControladorAnalitico(requisitos, planta);
 controlador = projetarControladorNelderMead(controlador_analitico);
+%controlador = projetarControladorCMAES(controlador_analitico);
 
+% para avaliar o controlador analitico:
+% avaliarControladorAnalitico(controlador_analitico, planta);
 
 %% Avaliar o controlodar de forma analítica (sistema aproximado)
 
 % Essa avaliação é sobre o controlador otimizado, porém com planta sem 
 % ruído e o zero-hold é aproximado por um atraso de T/2
-planta = obterPlanta();
-avaliarControladorAnalitico(controlador_analitico, planta);
+
+% para avaliar o controlador otimizado:
+avaliarControladorAnalitico(controlador, planta);
 
 
 %% Simulação
